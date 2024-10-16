0bec6b3a7c6254e4b1f953e0ac20f0e043b73cdb
BHUSHAN SANJAY ZADE <bhushanzade02@gmail.com>
2024-10-16 23:53:04 +0530

    Update 04_Print_value_pointer_variable.c

--- 05_Pointer/04_Print_value_pointer_variable.c
+++ 05_Pointer/04_Print_value_pointer_variable.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    int value = 42;   // Step 1: Declare and initialize an integer variable
+    int valu = 42;   // Step 1: Declare and initialize an integer variable
     int *ptr;         // Step 2: Declare a pointer to an integer
 
-    ptr = &value;     // Assign the address of 'value' to 'ptr'
+    ptr = &valu;     // Assign the address of 'value' to 'ptr'
 
     // Print the value of the pointer variable (which is the address it holds)
     printf("The value of the pointer variable 'ptr' is: %p\n", (void *)ptr);
