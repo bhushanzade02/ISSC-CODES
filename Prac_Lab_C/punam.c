b93ef548bcb1ad1d7a9b449519cd9ed2c6cf8ab1
BHUSHAN SANJAY ZADE <bhushanzade02@gmail.com>
2024-10-31 14:50:13 +0530

    daily practice

---
+++ Prac_Lab_C/punam.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+
+
+int sum=0;
+int a,b;
+printf("enter a number");
+scanf("%d",&a);
+printf("enter a number");
+scanf("%d",&b);
+printf("sum = %d",a+b);
+return 0;
+}
